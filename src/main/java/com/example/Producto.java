1940cb499503bd2b131bd11c6ffb5857e6edd81a
CarlosQuiroz25 <caquirozm@cesde.net>
2024-08-25 10:54:30 -0500

    Taller4

---
+++ src/main/java/com/example/Producto.java
@@ -0,0 +1,53 @@
+package com.example;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+        
+    }
+
+    public void mostrarInformacion(){
+        System.out.println(this.nombre);
+        System.out.println(this.precio);
+        System.out.println(this.stock);
+    }
+    public void actualizarStock(int stock){
+        if (stock >= 0) {
+            this.stock = stock;
+            System.out.println(this.stock);
+        } else{
+            System.out.println("El stock debe ser un valor positivo");
+        }
+    }
+
+    public void calcularPrecioTotal(){
+        double total = precio * stock;
+        System.out.println(total);
+    }
+
+    
+}
---
+++ src/main/java/com/example/Tienda.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class Tienda {
+    private ArrayList<Producto> producto = new ArrayList<>();
+
+    public void agregarProducto(Producto producto){
+        
+    }
+    
+
+}
